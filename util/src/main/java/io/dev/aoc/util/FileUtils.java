1e04235f069afb2a26d84835402176f36883f388
Istvan Oosterlinck <ioosterl@users.noreply.github.com>
2022-12-03 22:43:14 +0100

    Day 3 and some utils

---
+++ util/src/main/java/io/dev/aoc/util/FileUtils.java
@@ -0,0 +1,44 @@
+package io.dev.aoc.util;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class FileUtils {
+
+    public static <R> R processLinesAsStream(String filenameOnClasspath, Function<Stream<String>, R> linesProcessor) {
+        try (Stream<String> lines =
+                     Files.lines(
+                             getNullSafePath(filenameOnClasspath))) {
+            return linesProcessor.apply(lines);
+
+        }catch (IOException e) {
+            throw new IllegalArgumentException("Could not process file " + filenameOnClasspath, e);
+        }
+    }
+
+    public static <R> R processLinesAsList(String filenameOnClasspath, Function<List<String>, R> linesProcessor) {
+        try {
+            List<String> lines = Files.readAllLines(getNullSafePath(filenameOnClasspath));
+            return linesProcessor.apply(lines);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Could not process file " + filenameOnClasspath, e);
+        }
+    }
+
+    public static Path getNullSafePath(String filename) {
+        try {
+            return Paths.get(
+                    Objects.requireNonNull(
+                            FileUtils.class.getClassLoader().getResource(filename)).toURI());
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException("Could not find file " + filename, e);
+        }
+    }
+}
